cc4efef3af8e4cae1859b3a763274746925a4ead
Charlotte <44662646+Charlotte970304@users.noreply.github.com>
2023-06-12 05:56:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#coding=utf-8
 from flask import Flask, render_template, request, session, redirect, url_for,  jsonify
 import mysql.connector
 import os
@@ -10,8 +9,8 @@ app.secret_key = '123456789'
 # 建立與 MySQL 的連接
 database = mysql.connector.connect(
   host = "127.0.0.1",
-  user = "root",
-  password = "IM880319",
+  user = "yueh",
+  password = "oslab",
   database = "BookStore_DB" # 替換成你的資料庫名稱
 )
 
@@ -33,8 +32,7 @@ def login():
         query = "SELECT Account, Password FROM Member WHERE Account = %s"
         cursor.execute(query, (account,))
         user = cursor.fetchone()
-        
-        
+
         if user and user[1] == password:
             session['logged_in'] = True
             session['username'] = account
@@ -53,6 +51,7 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        print('yayaya')
         name = request.form.get('name')
         account = request.form.get('account')
         password = request.form.get('password')
@@ -82,72 +81,18 @@ def index():
     return render_template('index.html')
 
 
-@app.route('bookByclass')
-def bookByclass():
-    category = request.args.get('category', '')#哪個類型的書
-    #從上方的導覽列or下方選擇是哪種類型的書
-    #SELECT * FROM Book WHERE Cateogory LIKE %s
-    query = request.args.get('query', '')
-    cursor = database.cursor()
-    cursor.execute("SELECT * FROM Book WHERE Category LIKE %s", ('%' + category + '%',))
-    
-    books = cursor.fetchall()
-    # 準備空的書本列表
-    books_list = []
-    # 處理每一本書
-    for book in books:
-        # 取得 Author_ID 和 Publisher_ID
-        author_id = book[1]
-        publisher_id = book[2]
-
-        # 查找作者名
-        cursor.execute("SELECT Name FROM Author WHERE ID = %s", (author_id,))
-        author_name = cursor.fetchone()[0]
-
-        # 查找出版商名
-        cursor.execute("SELECT Name FROM Publisher WHERE ID = %s", (publisher_id,))
-        publisher_name = cursor.fetchone()[0]
-
-        # 組裝書本信息
-        book_dict = {
-            "BookName": book[0],
-            "AuthorName": author_name,
-            "PublisherName": publisher_name,
-            "Description": book[3],
-            "Language": book[4],
-            "Category": book[5],
-            "Price": book[6],
-            "Inventory": book[7]
-        }
-        # 添加到書本列表
-        books_list.append(book_dict)
-        print(books_list[0])
-
-    if 'logged_in' in session and session['logged_in']:
-        cursor = database.cursor()
-        cursor.execute('SELECT SUM(Quantity) FROM CartItems WHERE UserID = %s', (session['username'],))
-        try:
-            cart_count = int(cursor.fetchone()[0])
-        except:
-            cart_count = 0
-    else:
-        cart_count = 0
-    address = category + '.html'
-    return render_template(address, books = books_list, cart_count = cart_count)
-
 
 @app.route('/search')
 def search():
-    category = request.args.get('category', '')#哪個類型的書
-    #從上方的導覽列or下方選擇是哪種類型的書
-    #SELECT * FROM Book WHERE Cateogory LIKE %s
     query = request.args.get('query', '')
     cursor = database.cursor()
-    cursor.execute("SELECT * FROM Book WHERE Category LIKE %s", ('%' + category + '%',))
-    
+    cursor.execute("SELECT * FROM Book WHERE BookName LIKE %s", ('%' + query + '%',))
+
     books = cursor.fetchall()
+
     # 準備空的書本列表
     books_list = []
+
     # 處理每一本書
     for book in books:
         # 取得 Author_ID 和 Publisher_ID
@@ -175,7 +120,6 @@ def search():
         }
         # 添加到書本列表
         books_list.append(book_dict)
-        print(books_list[0])
 
     if 'logged_in' in session and session['logged_in']:
         cursor = database.cursor()
@@ -186,14 +130,10 @@ def search():
             cart_count = 0
     else:
         cart_count = 0
+
     return render_template('search.html', query=query, books=books_list,cart_count=cart_count)
 
-@app.before_request
-def before_request():
-    if 'logged_in' in session and session['logged_in']:
-        session['cart_total'] = sum(session.get('cart', {}).values())
-    else:
-        session['cart_total'] = 0
+
 
 @app.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
@@ -215,77 +155,13 @@ def add_to_cart():
     total_items = cursor.fetchone()
     
     return jsonify({'total_items': total_items})
-
-
-@app.route('/manage')
-def manage():
-    return render_template('manage.html')
-
-@app.route('/renew')
-def renew():
-    return render_template('renew.html')
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        
-        title = request.form.get('title')
-        description = request.form.get('description')
-        language = request.form.get('language')
-        category = request.form.get('category')
-        author_name = request.form.get('author')
-        publisher_name = request.form.get('publisher')
-        price = request.form.get('price')
-        amount = request.form.get('amount')
-        
-        cursor = database.cursor()
-
-        # 對於作者和出版商，先嘗試查詢
-        cursor.execute('SELECT id FROM Author WHERE Name = %s', (author_name,))
-        result = cursor.fetchone()
-        if result:
-            # 如果找到了結果，取得ID
-            author_id = result[0]
-        else:
-            # 如果沒有找到結果，插入新的記錄並取得新插入記錄的ID
-            cursor.execute('INSERT INTO Author (Name) VALUES (%s)', (author_name,))
-            database.commit()
-            cursor.execute('SELECT LAST_INSERT_ID()')
-            author_id = cursor.fetchone()[0]
-
-        cursor.execute('SELECT id FROM Publisher WHERE Name = %s', (publisher_name,))
-        result = cursor.fetchone()
-        if result:
-            publisher_id = result[0]
-        else:
-            cursor.execute('INSERT INTO Publisher (Name) VALUES (%s)', (publisher_name,))
-            database.commit()
-            cursor.execute('SELECT LAST_INSERT_ID()')
-            publisher_id = cursor.fetchone()[0]
-
-        # 假設您的Book表有相應的欄位
-        query = "INSERT INTO Book (BookName, Description, Language, Category, Author_ID, Publisher_ID, Price,Inventory) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)"
-        cursor.execute(query, (title, description, language, category, author_id, publisher_id, price,amount))
-        database.commit()
-
-        photo = request.files['file']
-        
-        # 檔案存在且格式允許，進行儲存
-        if photo and allowed_file(photo.filename):
-            photo.save(os.path.join(app.config['UPLOAD_FOLDER'], title+'.jpg'))
-
-
-        return redirect(url_for('manage')) 
-    else:
-        return render_template('upload.html')
     
-@app.route('/show_cart')
+@app.route('/cart')
 def show_cart():
     # 取出購物車中所有的商品
+    
     cursor = database.cursor()
-    if (session['username']):
-        cursor.execute('SELECT * FROM CartItems WHERE UserID = %s', (session['username'],))
+    cursor.execute('SELECT * FROM CartItems WHERE UserID = %s', (session['username'],))
     total_items = cursor.fetchall()
 
     cart_items=[]
@@ -310,17 +186,6 @@ def show_cart():
 
     # 將購物車中的商品和總價傳遞給模板
     return render_template('cart.html', cart_items=cart_items, total_price=total_price,cart_count=total_quantity)
-
-# delete the whole item
-@app.route('/delete_cart_item', methods=['POST'])
-def delete_cart_item():
-    cursor = database.cursor()
-    item_name = request.form.get('item_name')
-    cursor.execute('DELETE FROM CartItems WHERE UserID = %s AND Title = %s', (session['username'], item_name))
-    
-    return 'Cart item deleted successfully'
-   
-
 
 @app.route('/update-quantity', methods=['POST'])
 def update_quantity():
@@ -396,10 +261,10 @@ def order_process():
     # 在這裡進行後續的訂單處理
 
     return jsonify({'status': 'success', 'message': '訂單已收到'})
-  
- @app.route('/member',methods=['GET'])
+
+
+@app.route('/member',methods=['GET'])
 def member():
-#    return render_template('tables-data.html')
     return render_template('member.html')
 
 @app.route('/get_orders', methods=['POST'])
@@ -480,12 +345,6 @@ def get_images():
         if len(images)==5:
             break
     return jsonify(images)
-
-
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
